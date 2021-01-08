6c53096a17750f1e36c5c87868de88c4ea8545a2
nondefyde <nondefyde@gmail.com>
2021-01-08 19:30:07 +0100

    - revamped folders and project structure - added dependencies for react typescripts - added typescript config

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface AppProps {
+}
+
+const AppComponent: React.FC<AppProps> = () => {
+    return <div>
+        <h1>Hello</h1>
+    </div>;
+};
+
+export default AppComponent;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import AppComponent from './components/App/App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <AppComponent/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
